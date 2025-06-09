4bf74baddc29bfa570073fbad0af244fa358ada4
ahmet1407 <59695777+ahmet1407@users.noreply.github.com>
2025-06-09 14:45:23 +0300

    Update scraper_router.py

--- scraper_router.py
+++ scraper_router.py
@@ -1,4 +1,18 @@
-from scraper_manager import get_scraper_for_url
+from utils.detect_platform import detect_platform
+from utils.scrape_amazon import scrape_amazon_product
+from utils.scrape_hepsiburada import scrape_hepsiburada_product
+from utils.scrape_trendyol import scrape_trendyol_product
+from utils.serpapi_search import search_product_on_google
 
-def scrape_link(input_text):
-    return get_scraper_for_url(input_text)(input_text)
+def scrape_link(link_or_query):
+    platform = detect_platform(link_or_query)
+
+    if platform == "amazon":
+        return scrape_amazon_product(link_or_query)
+    elif platform == "hepsiburada":
+        return scrape_hepsiburada_product(link_or_query)
+    elif platform == "trendyol":
+        return scrape_trendyol_product(link_or_query)
+    else:
+        print("🔄 SerpAPI ile sorgulanıyor...")
+        return search_product_on_google(link_or_query)
